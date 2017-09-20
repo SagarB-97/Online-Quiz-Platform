eab11cc39e8cdcee7f3bca5624f7fe4f09878209
aneesh297 <aneesh297@gmail.com>
2017-09-20 21:34:28 +0530

    updated setup_db

--- setup_db.js
+++ setup_db.js
@@ -17,17 +17,49 @@ var CREATE_STUDENT_QUERY = "CREATE TABLE Student (student_id INT PRIMARY KEY AUT
 							phone varchar(20))";
 var CREATE_TESTSETTER_QUERY = "CREATE TABLE TestSetter (setter_id INT PRIMARY KEY AUTO_INCREMENT,\
 							login_id VARCHAR(50) UNIQUE)"; 
+var CREATE_CREDENTIALS_QUERY = "CREATE TABLE Credentials (login_id varchar(50) PRIMARY KEY,\
+								password varchar(50),\
+								privilege INT)";
 
+var CREATE_LISTOFQUIZ_QUERY = "CREATE TABLE ListOfQuizzes (quizid INT PRIMARY KEY,\
+								start_time datetime,\
+								end_time datetime,\
+								duration INT,\
+								total_marks INT,\
+								num_of_questions INT,\
+								leaderboardid INT,\
+								responsesid INT,\
+								testsetterid INT)";
+
+var CREATE_QUIZ_QUERY = "CREATE TABLE Quiz (qno INT PRIMARY KEY,\
+						 question varchar(50),\
+						 answer varchar(255),\
+						 qtype varchar(20),\
+						 marks INT)";
+
+var CREATE_LEADERBOARD_QUERY = "CREATE TABLE Leaderboard (Rank INT PRIMARY KEY,\
+								student varchar(100))";
+
+var CREATE_RESPONSES_QUERY = "CREATE TABLE Responses (student_id INT,\
+							  answers varchar(255),\
+							  marks_alloted INT)";
 
 function initStatements(err){
 	if (err) throw err;
 	console.log("Connected!");
 
+
 	// Create DB
 	con.query(CREATE_DB_QUERY, function (err, result) {
+		//console.log(err);
+		if(err.code === "ER_DB_CREATE_EXISTS")
+		{
+			console.log("Database Already Exists");	
+		}
 		if (err) throw err;
 		console.log("Database Created");
 	});
+
 
 	// Use DB
 	con.query(USE_DB_QUERY, function (err, result) {
@@ -35,10 +67,46 @@ function initStatements(err){
 		console.log("Using DB");
 	});
 
-	// Use DB
+	// Create Student Table
 	con.query(CREATE_STUDENT_QUERY, function (err, result) {
 		if (err) throw err;
 		console.log("Student Table created");
+	});
+
+	// Create TestSetter Table
+	con.query(CREATE_TESTSETTER_QUERY, function (err, result) {
+		if (err) throw err;
+		console.log("TestSetter Table created");
+	});
+
+	// Create Credentials Table
+	con.query(CREATE_CREDENTIALS_QUERY, function (err, result) {
+		if (err) throw err;
+		console.log("Credentials Table created");
+	});
+
+	// Create List of Quizzes Table
+	con.query(CREATE_LISTOFQUIZ_QUERY, function (err, result) {
+		if (err) throw err;
+		console.log("ListOfQuizzes Table created");
+	});
+
+	// Create Quiz Table
+	con.query(CREATE_QUIZ_QUERY, function (err, result) {
+		if (err) throw err;
+		console.log("Quiz Table created");
+	});
+
+	//Create Leaderboard Table
+	con.query(CREATE_LEADERBOARD_QUERY, function (err, result) {
+		if (err) throw err;
+		console.log("Leaderboard Table created");
+	});
+
+	//Create Responses Table
+	con.query(CREATE_RESPONSES_QUERY, function (err, result) {
+		if (err) throw err;
+		console.log("Responses Table created");
 	});
 
 }

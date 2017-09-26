8e4bd5bad65c6c337d78c69bbb6e0b5de9069594
Sagar Bharadwaj <sagarbharadwaj50@gmail.com>
2017-09-26 15:06:15 +0530

    Added favicon and changed title

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var favicon = require('serve-favicon');
 
 var login = require('./routes/login');
 var users = require('./routes/users');
@@ -17,6 +18,7 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));

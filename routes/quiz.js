a71378f4518277dc3efbb3f6601c62e9cb76c87a
Sagar Bharadwaj <sagarbharadwaj50@gmail.com>
2017-10-29 18:34:09 +0530

    Updated responses on database. Calculation of marks obtained done

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var selecter = require('../database_handlers/selectQuery.js');
+var inserter = require('../database_handlers/insertQuery.js');
 
-
+function q(str){
+    return "'"+str+"'";
+}
 router.get('/:quizid', function(req, res, next){
     var quizid = req.params.quizid;
 
@@ -26,7 +29,58 @@ router.get('/:quizid', function(req, res, next){
 });
 
 router.post('/submit_answer/:quizid',function(req,res,next){
-    var student_responses = req.body;
+    var quizid = req.params.quizid;
+    var student_responses = req.body.responses;
+    var student_id = req.cookies.login;
+
+    // var INSERT_QUERY_RESPONSES = "INSERT INTO Responses values ("+
+    //                     q(student_id)+","+
+    //                     quizid+","+
+    //                     q(student_responses)+",NULL);";
+    // inserter(INSERT_QUERY_RESPONSES,function(){
+    //     var SELECT_QUERY_ANSWERS = "SELECT answer,marks FROM Quiz WHERE quizid = "+quizid+
+    //         " ORDER BY qno;";
+    //     selecter(SELECT_QUERY_ANSWERS,res,function(result,response){
+    //         var student_ans = [];
+    //         var correct_ans = [];
+    //         var marks = [];
+    //         for(var i = 0;i<result.length;i++){
+    //             correct_ans.push(result[i].answer);
+    //             marks.push(result[i].marks);
+    //         }
+    //         student_ans = student_responses.split('$');
+    //         var tot_marks = 0;
+    //         for(var i = 0;i<student_ans.length;i++){
+    //             if(student_ans[i]==correct_ans[i])
+    //                 tot_marks+=marks[i];
+    //         }
+    //         console.log("tot_marks = "+tot_marks);
+    //     });
+    // });
+
+
+    var SELECT_QUERY_ANSWERS = "SELECT answer,marks FROM Quiz WHERE quizid = "+quizid+
+             " ORDER BY qno;";
+    selecter(SELECT_QUERY_ANSWERS,res,function(result,response){
+        var student_ans = [];
+        var correct_ans = [];
+        var marks = [];
+        for(var i = 0;i<result.length;i++){
+            correct_ans.push(result[i].answer);
+            marks.push(result[i].marks);
+        }
+        student_ans = student_responses.split('$');
+        var tot_marks = 0;
+        for(var i = 0;i<student_ans.length;i++){
+         if(student_ans[i]==correct_ans[i])
+            tot_marks+=marks[i];
+        }
+        var INSERT_QUERY_RESPONSES = "INSERT INTO Responses values ("+
+                            q(student_id)+","+
+                            quizid+","+
+                            q(student_responses)+"," + tot_marks + ");";
+        inserter(INSERT_QUERY_RESPONSES);
+    });
     res.render('student/Quiz/submit_answer');
 });
 

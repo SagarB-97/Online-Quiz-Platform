52ca8ee333a4693da64bf5fede60e80095f5ff14
Aneesh <aneesh297@gmail.com>
2017-10-31 19:47:43 +0530

    Added new fields in Student user profile

--- routes/dashboard_student.js
+++ routes/dashboard_student.js
@@ -36,7 +36,7 @@ router.get('/user_profile',function(req,res,next){
     var login_id = req.cookies.login;
     var SELECT_QUERY = "SELECT * FROM Student WHERE login_id = " + q(login_id) + ";";
     selecter(SELECT_QUERY,res,function(result,res){
-        res.render('student/Dashboard/user_profile',result[0]);
+        res.render('student/Dashboard/user_profile',{result:result[0]});
     });
 });
 
